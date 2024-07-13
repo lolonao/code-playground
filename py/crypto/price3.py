ba378a07c56f71119c87a64bf0b99366ca2e931e
lolonao <lolonao47@gmail.com>
2024-07-14 04:53:57 +0800

    update

--- py/crypto/price3.py
+++ py/crypto/price3.py
@@ -3,6 +3,11 @@ import requests
 from pydantic import BaseModel, field_validator
 import asyncio
 
+import schedule
+import time
+import datetime as dt
+
+
 # APIエンドポイントURL
 url = "https://api.pro.coins.ph/openapi/v1/ticker/price?symbols=[BTCPHP,DOGEPHP,BONKPHP,SOLPHP]"
 
@@ -61,13 +66,30 @@ async def get_price():
         # エラー処理
         print(f"エラーが発生しました: {error}")
 
+def job():
+    print("1分おきに実行します。")
+    print(dt.datetime.now())
+    import asyncio
+    asyncio.run(main())
+
 # メイン処理 (非同期)
 async def main():
     """
     非同期的に価格取得処理を実行する
     """
     await get_price()
+    print("---------")
+
 
 if __name__ == "__main__":
     import asyncio
-    asyncio.run(main())
+    
+    # 毎日午前9時にジョブを実行
+    # schedule.every().day.at("09:00").do(job)
+    # 1分お気に実行
+    schedule.every(1).minutes.do(job)
+
+    while True:
+        # スケジュールされたジョブを実行
+        schedule.run_pending()
+        time.sleep(10)
