20122b90e45324e543ca4ccc354da7d4e39e15f9
lolonao <lolonao47@gmail.com>
2024-07-18 07:40:35 +0800

    update code

--- py/schedule-playground/croniter-demo.py
+++ py/schedule-playground/croniter-demo.py
@@ -3,6 +3,9 @@ from croniter import croniter
 from datetime import datetime
 
 base = datetime(2024, 7, 14, 16, 3)
-iter = croniter('*/5 * * * *', base)  # every 5 minutes
+iter = croniter('*/1 * * * *', base)  # every 5 minutes
 print(iter.get_next(datetime))
 
+numbers = [1, 1, 2, 2, 3, 3, 3]
+unique = {num for num in numbers}
+print(unique)
