781fa4f011443e2ed4cd9f567710020797f0907a
lolonao <lolonao47@gmail.com>
2024-07-20 16:35:47 +0800

    icecream関連の更新

---
+++ py/learning-python/replacement_print_with_icecream_ic.py
@@ -0,0 +1,5 @@
+"""参考情報
+- [Debugging 101: Replace print() with icecream ic() - YouTube](https://www.youtube.com/watch?v=JJ9zZ8cyaEk)
+- [IceCream使い方まとめ #Python - Qiita](https://qiita.com/miyashiiii/items/c0c4c709e096937df906)
+- [gruns/icecream: 🍦 Never use print() to debug again.](https://github.com/gruns/icecream)
+"""
